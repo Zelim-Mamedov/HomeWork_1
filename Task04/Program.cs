2b59fb9df4a1bbc836c1ceb6b668578e278edea3
Zelim <zelya140489@gmail.com>
2023-01-20 10:50:51 +0600

    Добавили и изменили программу

---
+++ Task04/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на 
+// вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.WriteLine("Введите первое число:"); 
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine($"{num1}, {num2}, {num3} -> {max}!");
+
+// ВТОРОЙ ВАРИАНТ НАШЕЛ В ИНТЕРНЕТЕ, НО Я ЕГО НЕ ПОНЯЛ!!! 
+// НО ВИДИМО МЫ ЭТО ЕЩЕ НЕ ПРОХОДИЛИ!
+// Я ЭТОТ КОД НА БУДУЩЕЕ СОХРАНИЛ, ЧТОБЫ ИЗУЧИТЬ!
+
+// string[] i = Console.ReadLine().Split(' ');
+// int a = Convert.ToInt32(i[0]);
+// int b = Convert.ToInt32(i[1]);
+// int c = Convert.ToInt32(i[2]);
+// Console.WriteLine(Math.Max(a, Math.Max(b, c)));
---
+++ Task06/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double num_2 = num % 2;
+if (num_2 == 0)
+    Console.WriteLine($"{num} -> да");
+else
+    Console.WriteLine($"{num} -> нет");
